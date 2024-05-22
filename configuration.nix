82dc154e0a68c5674adaa5608680a8d290a24d8a
Victor Quintana <435victorjavier@gmail.com>
2024-05-22 15:09:40 -0600

    add packages

--- configuration.nix
+++ configuration.nix
@@ -124,7 +124,10 @@
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     asusctl
+    alejandra
     gitAndTools.git-credential-manager
+    libnotify
+    jq
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
