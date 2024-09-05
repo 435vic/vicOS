56ab398c118deb6caed254a29058d3d923a64d58
Victor Quintana <435victorjavier@gmail.com>
2024-09-05 21:47:54 +0000

    formatting and renix

---
+++ scripts/renix.sh
@@ -0,0 +1,41 @@
+set -e
+
+pushd /etc/nixos
+
+if [ "$1" != "-b" ]; then
+	$EDITOR ${1:-.}
+	if [ "$?" -ne 0 ]; then
+		echo "renix was cancelled :("
+		popd
+		exit 1
+	fi
+fi
+
+if (git status '*.nix' --porcelain | grep '^??'); then
+	echo "There are untracked files."
+	echo "Add them with 'git add' and do renix -b to skip editing."
+	exit 1
+fi
+
+if git diff --quiet '*.nix'; then
+	echo "No changes detected, exiting."
+	popd
+	exit 0
+fi
+
+nix fmt . &>/dev/null \
+	|| ( nix fmt . ; echo "Formatting failed!" && exit 1)
+
+git diff -U0 '*.nix'
+
+echo "reNixing now"
+
+sudo nixos-rebuild switch --flake . >nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1)
+
+geninfo=$(nixos-rebuild list-generations --json | jq -r '"Gen \(.[0].generation) NixOS \(.[0].nixosVersion) Kernel \(.[0].kernelVersion)"')
+
+git commit -am "$geninfo"
+
+popd
+
+notify-send -e "reNix successful :D" --icon=software-update-available
