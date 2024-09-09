1456f5213eb10fd9ae5cb1a6ff916bcbf475c8f7
Victor Quintana <435victorjavier@gmail.com>
2024-09-09 14:34:46 -0600

    Gen 48 NixOS 24.11.20240904.ad416d0 Kernel 6.10.7

--- thunkbox/default.nix
+++ thunkbox/default.nix
@@ -58,6 +58,7 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
+  services.xserver.videoDrivers = [ "amdgpu" ];
 
   #services.displayManager.sddm.enable = true;
   #services.displayManager.sddm.theme = "catppuccin-mocha";
@@ -100,14 +101,14 @@
   services.printing.enable = true;
 
   # temp fix for vulkan issues
-  #hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-  #  version = "555.58.02";
-  #  sha256_64bit = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
-  #  sha256_aarch64 = "sha256-wb20isMrRg8PeQBU96lWJzBMkjfySAUaqt4EgZnhyF8=";
-  #  openSha256 = "sha256-8hyRiGB+m2hL3c9MDA/Pon+Xl6E788MZ50WrrAGUVuY=";
-  #  settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
-  #  persistencedSha256 = "sha256-a1D7ZZmcKFWfPjjH1REqPM5j/YLWKnbkP9qfRyIyxAw=";
-  #};
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    version = "555.58.02";
+    sha256_64bit = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
+    sha256_aarch64 = "sha256-wb20isMrRg8PeQBU96lWJzBMkjfySAUaqt4EgZnhyF8=";
+    openSha256 = "sha256-8hyRiGB+m2hL3c9MDA/Pon+Xl6E788MZ50WrrAGUVuY=";
+    settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+    persistencedSha256 = "sha256-a1D7ZZmcKFWfPjjH1REqPM5j/YLWKnbkP9qfRyIyxAw=";
+  };
 
   # Enable sound with pipewire.
   #sound.enable = true;
@@ -130,6 +131,7 @@
   powerManagement.enable = true;
   hardware.nvidia.powerManagement.finegrained = true;
   hardware.nvidia.powerManagement.enable = true;
+  hardware.nvidia.modesetting.enable = true;
 
   users.users.vico = {
     isNormalUser = true;
