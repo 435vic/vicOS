b40f013bcf6e4b43b56de122cfee731024ba6eeb
Victor Quintana <435victorjavier@gmail.com>
2024-09-14 11:29:37 -0600

    add nodejs flake

--- flake.nix
+++ flake.nix
@@ -63,6 +63,11 @@
           path = ./templates/rust;
           description = "A basic flake for rust development using direnv and fenix";
         };
+
+        node_22 = {
+          path = ./templates/node;
+          description = "Node.js development with pnpm";
+        };
       };
 
       flake.nixosConfigurations = {
