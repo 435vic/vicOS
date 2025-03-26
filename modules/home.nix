159e4bf3ac48d7622bb8ce92bc3097817f7b8d50
Victor Quintana <435victorjavier@gmail.com>
2025-03-25 18:47:33 -0600

    fix xdg/home modules

--- modules/home.nix
+++ modules/home.nix
@@ -5,7 +5,14 @@
   lib,
   ...
 }:
-with lib; {
+with lib; let
+  mkStringOpt = default: description: mkOption {
+    type = types.str;
+    inherit default description;
+  };
+  homeDir = config.vicos.user.home;
+  cfg = config.home;
+in {
   options.home = {
     configFile = mkOption {
       type = types.attrs;
@@ -18,6 +25,13 @@ with lib; {
       default = {};
       description = "Files to place in $XDG_DATA_HOME";
     };
+
+    binDir     = mkStringOpt "${homeDir}/.local/bin" "";
+    cacheDir   = mkStringOpt "${homeDir}/.cache" "";
+    configDir  = mkStringOpt "${homeDir}/.config" "";
+    dataDir    = mkStringOpt "${homeDir}/.local/share" "";
+    stateDir   = mkStringOpt "${homeDir}/.local/state" "";
+    fakeDir    = mkStringOpt "${homeDir}/.local/user" "";
   };
 
   config = let
@@ -73,6 +87,16 @@ with lib; {
           ln -s ${config.vicos.flake.path}/config/${path}/$path $out/$path
         done
       '';
+
+    environment.sessionVariables = mkOrder 10 {
+      XDG_BIN_HOME    = cfg.binDir;
+      XDG_CACHE_HOME  = cfg.cacheDir;
+      XDG_CONFIG_HOME = cfg.configDir;
+      XDG_DATA_HOME   = cfg.dataDir;
+      XDG_STATE_HOME  = cfg.stateDir;
+      XDG_FAKE_HOME = cfg.fakeDir;
+      XDG_DESKTOP_DIR = cfg.fakeDir;
+    };
 
     home-manager = {
       useUserPackages = true;
