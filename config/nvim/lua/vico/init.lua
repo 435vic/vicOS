8983d98bdc306a74db2ed52195789962a62f7bbd
Victor Quintana <435victorjavier@gmail.com>
2025-03-25 18:47:50 -0600

    nvim remaps and autocmds

--- config/nvim/lua/vico/init.lua
+++ config/nvim/lua/vico/init.lua
@@ -8,6 +8,20 @@
 require('vico.general')
 require('vico.remap')
 
+local VicosGroup = vim.api.nvim_create_augroup('vico', {})
+
+-- Highlight text on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+    group = VicosGroup,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+           higroup = 'IncSearch',
+           timeout = 60,
+        })
+    end,
+})
+
 -- ▄▄▄▄  █ █  ▐▌  ▄ ▄▄▄▄   ▄▄▄
 -- █   █ █ ▀▄▄▞▘  ▄ █   █ ▀▄▄
 -- █▄▄▄▀ █        █ █   █ ▄▄▄▀
--- config/nvim/lua/vico/remap.lua
+++ config/nvim/lua/vico/remap.lua
@@ -1,3 +1,8 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]], { desc = "Yank into system clipboard" })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank line into system clipboard" })
+
+vim.keymap.set({"n", "v"}, "<leader>d", [[\"_d]])
+
