d678b27c14d2ff45166bb999295bcbc4544005e8
Victor Quintana <435victorjavier@gmail.com>
2025-04-21 00:04:12 -0600

    add sesh for tmux session management

--- modules/shell/tmux.nix
+++ modules/shell/tmux.nix
@@ -8,6 +8,12 @@
 in {
   options.vicos.shell.tmux = {
     enable = mkEnableOption "tmux";
+    sesh = {
+      enable = mkOption {
+        type = types.bool;
+        default = true;
+      };
+    };
     plugins = mkOption {
       default = [ ];
       type = types.listOf types.package;
@@ -16,27 +22,51 @@ in {
     };
   };
 
-  config = mkIf cfg.enable {
-    environment.variables.TMUX_HOME = "$XDG_CONFIG_HOME/tmux";
+  config = mkIf cfg.enable (mkMerge [
+    {
+      environment.variables.TMUX_HOME = "$XDG_CONFIG_HOME/tmux";
 
-    vicos.user.packages = [ pkgs.unstable.tmux ];
+      vicos.user.packages = [ pkgs.unstable.tmux ];
 
-    environment.etc."tmux.conf".text = ''
-      source-file $TMUX_HOME/tmux.conf
+      environment.etc."tmux.conf".text = ''
+        source-file $TMUX_HOME/tmux.conf
 
-      ${lib.optionalString (cfg.plugins != [ ]) ''
-        ${lib.concatMapStringsSep "\n" (plugin: "run-shell ${plugin.rtp}") cfg.plugins}
-      ''}
-    '';
+        ${lib.optionalString (cfg.plugins != [ ]) ''
+          ${lib.concatMapStringsSep "\n" (plugin: "run-shell ${plugin.rtp}") cfg.plugins}
+        ''}
+      '';
 
-    vicos.shell.tmux.plugins = with pkgs.tmuxPlugins; [
-      yank
-      rose-pine
-    ];
+      vicos.shell.tmux.plugins = with pkgs.tmuxPlugins; [
+        yank
+        rose-pine
+      ];
 
-    home.configFile.tmux = {
-      source = config.lib.vicos.dirFromConfig "tmux";
-      recursive = true;
-    };
-  };
+      home.configFile.tmux = {
+        source = config.lib.vicos.dirFromConfig "tmux";
+        recursive = true;
+      };
+    }
+    (mkIf cfg.sesh.enable {
+      vicos.user.packages = [ pkgs.unstable.sesh ];
+      programs.fish.interactiveShellInit = mkAfter ''
+        bind ctrl-f 'sesh connect (sesh list | fzf)'
+      '';
+      environment.etc."tmux.conf".text = mkAfter ''
+        bind-key "T" run-shell "sesh connect \"$(
+          sesh list --icons | fzf-tmux -p 80%,70% \
+            --no-sort --ansi --border-label ' sesh ' --prompt '⚡  ' \
+            --header '  ^a all ^t tmux ^g configs ^x zoxide ^d tmux kill ^f find' \
+            --bind 'tab:down,btab:up' \
+            --bind 'ctrl-a:change-prompt(⚡  )+reload(sesh list --icons)' \
+            --bind 'ctrl-t:change-prompt(🪟  )+reload(sesh list -t --icons)' \
+            --bind 'ctrl-g:change-prompt(⚙️  )+reload(sesh list -c --icons)' \
+            --bind 'ctrl-x:change-prompt(📁  )+reload(sesh list -z --icons)' \
+            --bind 'ctrl-f:change-prompt(🔎  )+reload(fd -H -d 2 -t d -E .Trash . ~)' \
+            --bind 'ctrl-d:execute(tmux kill-session -t {2..})+change-prompt(⚡  )+reload(sesh list --icons)' \
+            --preview-window 'right:55%' \
+            --preview 'sesh preview {}'
+        )\""
+      '';
+    })
+  ]);
 }
