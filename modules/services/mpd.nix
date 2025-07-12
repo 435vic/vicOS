db8eebc23f4c6ca716ed0c70e74b0a4cb31213ff
Victor Quintana <435victorjavier@gmail.com>
2025-07-12 17:13:39 -0600

    add mpd-mpris

--- modules/services/mpd.nix
+++ modules/services/mpd.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: with lib; let
   cfg = config.vicos.services.mpd;
@@ -11,25 +12,46 @@ in {
       default = config.vicos.desktop.enable;
       description = "Whether to enable mpd";
     };
+    mpris.enable = mkEnableOption "mpris compatibility";
   };
 
-  config = mkIf cfg.enable {
-    services.mpd = {
-      enable = true;
-      musicDirectory = mkDefault "/home/${config.vicos.user.name}/Music";
-      extraConfig = ''
-        audio_output {
-          type "pipewire"
-          name "default output"
-        }
-      '';
+  config = mkMerge [
+    (mkIf cfg.enable {
+      services.mpd = {
+        enable = true;
+        musicDirectory = mkDefault "/home/${config.vicos.user.name}/Music";
+        extraConfig = ''
+          audio_output {
+            type "pipewire"
+            name "default output"
+          }
+        '';
 
-      # run mpd as main user, as pipewire doesn't normally run as system
-      user = config.vicos.user.name; 
-    };
+        # run mpd as main user, as pipewire doesn't normally run as system
+        user = config.vicos.user.name; 
+      };
 
-    systemd.services.mpd.environment = {
-      XDG_RUNTIME_DIR = "/run/user/${toString config.vicos.user.uid}";
-    };
-  };
+      systemd.services.mpd.environment = {
+        XDG_RUNTIME_DIR = "/run/user/${toString config.vicos.user.uid}";
+      };
+    })
+    (mkIf (cfg.enable && cfg.mpris.enable) {
+      systemd.user.services.mpd-mpris = {
+        Install = { WantedBy = [ "default.target" ]; };
+
+        Unit = {
+          Description = "An MPRIS protocol implementation for the MPD music player";
+          After = "mpd.service";
+        };
+
+        Service = {
+          Type = "dbus";
+          Restart = "on-failure";
+          RestartSec = "5s";
+          ExecStart = "${pkgs.mpd-mpris}/bin/mpd-mpris -no-instance";
+          BusName = "org.mpris.MediaPlayer2.mpd";
+        };
+      };
+    })
+  ];
 }
