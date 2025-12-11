0108c73cd91e9e05f747d576fb11caff2d62a41d
Victor Quintana <435victorjavier@gmail.com>
2025-12-11 20:27:21 +0100

    feat: add gnome keyring in hyprland

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -124,6 +124,16 @@ in
       MOZ_ENABLE_WAYLAND = "1";
     };
 
+    services.gnome.gnome-keyring.enable = true;
+    security.pam.services = {
+      greetd.enableGnomeKeyring = true;
+      login.enableGnomeKeyring = true;
+    };
+    services.dbus.packages = [
+      pkgs.gnome-keyring
+      pkgs.gcr
+    ];
+
     hardware.graphics = {
       enable = true;
       enable32Bit = true;
