f8697df83ed1c5df202ec964d8a875155b8db8dc
Victor Quintana <435victorjavier@gmail.com>
2026-04-06 12:38:25 -0600

    restore old helium packaging

--- packages/helium.nix
+++ packages/helium.nix
@@ -10,10 +10,9 @@ let
   pname = "helium";
   version = "0.10.8.1";
   src = fetchurl {
-    url = "https://github.com/imputnet/helium-linux/releases/download/0.10.8.1/helium-0.10.8.1-x86_64_linux.tar.xz";
-    hash = "sha256-qu0GvCbg4Eq1cHrTerOzH98KkL0FCSVQgLe+QWyH9d4=";
+    url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64.AppImage";
+    hash = "sha256-pN/Iw1ANggDOxxFb2CN436qbcrs8/bDcEqjZC80grQs=";
   };
-
 
   contents = appimageTools.extract {
     inherit pname version src;
