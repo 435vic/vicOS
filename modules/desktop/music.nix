73988c8a3110dad46e11c8b15142175681a4ddf1
Victor Quintana <435victorjavier@gmail.com>
2026-04-25 23:34:55 -0600

    fix(mpd): systemd stuff

--- modules/desktop/music.nix
+++ modules/desktop/music.nix
@@ -15,36 +15,30 @@
   };
 
   systemd.user.services.mpd = {
-    Unit = {
-      Description = "Music Player Daemon";
-      After = [
-        "network.target"
-        "sound.target"
-        "mpd.socket"
-      ];
-      Requires = [ "mpd.socket" ];
-    };
+    description = "Music Player Daemon";
+    after = [
+      "network.target"
+      "sound.target"
+      "mpd.socket"
+    ];
+    requires = [ "mpd.socket" ];
 
-    Service = {
+    serviceConfig = {
       Type = "notify";
-      ExecStart = "${pkgs.mpd}/bin/mpd --no-daemon $XDG_CONFIG_HOME/mpd/mpd.conf";
+      ExecStart = "${pkgs.mpd}/bin/mpd --no-daemon %h/.config/mpd/mpd.conf";
     };
   };
 
   systemd.user.sockets.mpd = {
-    Socket = {
+    wantedBy = [ "sockets.target" ];
+
+    socketConfig = {
       ListenStream = [ "127.0.0.1:6600" "%t/mpd/socket" ];
       Backlog = 5;
       KeepAlive = true;
     };
-
-    Install.WantedBy = [ "sockets.target" ];
   };
 
   home.configFile."mpd/mpd.conf".source =
-    config.lib.vicos.stash "mpd/mpd.conf";
-
-  systemd.services.mpd.environment = {
-    XDG_RUNTIME_DIR = "/run/user/${toString config.vicos.user.uid}";
-  };
+    config.lib.vicos.stash "config/mpd/mpd.conf";
 }
