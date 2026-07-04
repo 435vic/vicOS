7f81e9a56bb541093bd5e0d51239925ae52d8e36
Victor Quintana <435victorjavier@gmail.com>
2026-07-03 23:23:35 -0600

    bump helium

--- packages/helium.nix
+++ packages/helium.nix
@@ -8,10 +8,10 @@
 }:
 let
   pname = "helium";
-  version = "0.12.4.1";
+  version = "0.14.2.1";
   src = fetchurl {
     url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64.AppImage";
-    hash = "sha256-OgS8HkLBseFrEhNFJxMwp1bg0gzPdfY1VaySAAp7vq0=";
+    hash = "sha256-JQQ5K+WUZ3ly5mklNPGZWDTlq+mJqqS02oJgC3in5U0=";
   };
 
   contents = appimageTools.extract {
